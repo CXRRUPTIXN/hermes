1819c9ad48dc67e08f60ed0c000b86682f0a39e1
CXRRUPTIXN <CXRRUPTIXNdev@gmail.com>
2024-04-27 10:29:37 -0500

    Config changes

--- front_end/main.py
+++ front_end/main.py
@@ -3,7 +3,6 @@ from flask_bootstrap import Bootstrap
 
 # def create_app(): # TODO: Factor this into its own function later, setting as a global var for now
 app = Flask(__name__)
-Bootstrap(app)
 # return app
 
 @app.route("/")
